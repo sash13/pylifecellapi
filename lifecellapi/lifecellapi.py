af1e1b60cca083d0267f1bf884d680a53e87db3f
Alexander <trash@openidev.ru>
2016-02-04 16:44:37 +0200

    Add request method

--- lifecellapi/lifecellapi.py
+++ lifecellapi/lifecellapi.py
@@ -2,6 +2,8 @@
 #!/usr/bin/env python
 
 import urllib
+import requests
+import random
 import hmac
 import hashlib
 import base64
@@ -10,8 +12,13 @@ logger = logging.getLogger('LifeApi')
 
 from config import settings
 
-delimiter = '?'
-sign_attr = '&signature='
+DELIMITER = '?'
+SIGNATURE = '&signature='
+USER_AGENTS = ('Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; Touch)',
+               'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; en-us; Silk/1.1.0-80) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16 Silk-Accelerated=true',
+               'Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10',
+               'BlackBerry9700/5.0.0.862 Profile/MIDP-2.1 Configuration/CLDC-1.1 VendorID/331 UNTRUSTED/1.0 3gpp-gba',
+              )
 
 class LifecellApi():
   """Class for acess lifecell api"""
@@ -24,7 +31,19 @@ class LifecellApi():
 
   def createSignedUrl(self, method, params):
     query = urllib.urlencode(params)
-    string = ''.join((method, delimiter, query, sign_attr))
+    string = ''.join((method, DELIMITER, query, SIGNATURE))
     digest = hmac.new(self.settings['application_key'], string, hashlib.sha1).digest()
     sign = base64.b64encode(digest) 
     return ''.join((self.settings['api_url'], string, sign))
+
+  def request(self, method, params):
+    logger.debug('{0} request: {1}'.format(method, params))
+    url = self.createSignedUrl(method, params)
+    try:
+      return requests.get(url, headers={'User-Agent': random.choice(USER_AGENTS)}).text
+    except requests.exceptions.Timeout as e:
+      logger.debug('Request timeout.')
+      raise e
+    except requests.exceptions.RequestException as e:
+      logger.debug('Request error.')
+      raise e
--- test_lifecellapi.py
+++ test_lifecellapi.py
@@ -7,10 +7,16 @@ from  lifecellapi import LifecellApi
 class LifecellApiTest(unittest.TestCase):
   def setUp(self):
     self.api = LifecellApi('380931234567', '123456')
+    self.test_req = { 'accessKeyCode': self.api.settings['access_key_code'], 'msisdn': self.api.num, 'superPassword': self.api.pwd}
 
   def testSignedUrl(self):
-    req = { 'accessKeyCode': self.api.settings['access_key_code'], 'msisdn': self.api.num, 'superPassword': self.api.pwd}
-    answer = self.api.createSignedUrl('signIn', req)
+    answer = self.api.createSignedUrl('signIn', self.test_req)
     desired = 'https://api.life.com.ua/mobile/signIn?msisdn=380931234567&superPassword=123456&accessKeyCode=7&signature=JpDJ2UD8OHsZL19S9UOEePIRaN8='
     self.assertEqual(answer, desired,
                     'incorrect sign string')
+
+  def testRequest(self):
+    answer = self.api.request('signIn', self.test_req)
+    desired = '''<?xml version="1.0" encoding="UTF-8"?>\n<response method="signIn"><responseCode>-10</responseCode></response>'''
+    self.assertEqual(answer, desired,
+                    'incorrect answer')
