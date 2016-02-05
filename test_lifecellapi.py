f1301e546f68307ceb531f830c2386f12c1a4bc9
Alexander <trash@openidev.ru>
2016-02-05 20:10:27 +0200

    Add new unittests

--- test_lifecellapi.py
+++ test_lifecellapi.py
@@ -2,26 +2,51 @@
 #!/usr/bin/env python
 
 import unittest
-from lifecellapi import LifecellSession
+from lifecellapi import (LifecellSession, LifecellApi,
+                         LifecellApiWrongMethodError, LifecellApiError
+                        )
 
 
 class LifecellApiTest(unittest.TestCase):
 
   def setUp(self):
-    self.api = LifecellSession('380931234567', '123456')
+    self.s = LifecellSession('380931234567', '123456')
+    self.api = LifecellApi(self.s)
     self.req = {
-      'accessKeyCode': self.api.settings['access_key_code'],
-      'msisdn': self.api.num,
-      'superPassword': self.api.pwd}
+      'accessKeyCode': self.s.settings['access_key_code'],
+      'msisdn': self.s.num,
+      'superPassword': self.s.pwd
+      }
 
   def testSignedUrl(self):
-    answer = self.api.createSignedUrl('signIn', self.req)
+    answer = self.s.createSignedUrl('signIn', self.req)
     desired = 'https://api.life.com.ua/mobile/signIn?msisdn=380931234567&superPassword=123456&accessKeyCode=7&signature=JpDJ2UD8OHsZL19S9UOEePIRaN8='
     self.assertEqual(answer, desired,
              'incorrect sign string')
 
   def testRequest(self):
-    answer = self.api.request('signIn', self.req)
+    answer = self.s.request('signIn', self.req)
     desired = '''<?xml version="1.0" encoding="UTF-8"?>\n<response method="signIn"><responseCode>-10</responseCode></response>'''
     self.assertEqual(answer, desired,
              'incorrect answer')
+
+  def testApiWrongRequest(self):
+    try:
+      self.api.wrongApi()
+    except LifecellApiWrongMethodError, e:
+      pass
+    except e:
+      self.fail('Unexpected exception throw:', e)
+    else:
+      self.fail('LifecellApiWrongMethodError not throw')
+      
+  def testApiErrorRequest(self):
+    try:
+      self.api.getToken()
+    except LifecellApiError, e:
+      pass
+    except e:
+      self.fail('Unexpected exception throw:', e)
+    else:
+      self.fail('LifecellApiError not throw')
+
